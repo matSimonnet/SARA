766843bbcb94110c8e0c16f1985c7d50b2ffd739
kvlch <kvlchankrachang@gmail.com>
2014-03-26 14:20:56 +0100

    adding name to WayPoint class

--- src/orion/ms/sara/WP.java
+++ src/orion/ms/sara/WP.java
@@ -1,18 +1,30 @@
 package orion.ms.sara;
 //a waypoint class containing each latitude, longitude, distance and bearing
 public class WP {
-		//positions : 
+		//name
+		private String name = "";
+		//positions  
 		private String latitude = "";
 		private String longitude = "";
+		//distance and bearing
 		private double distance = 0.0;
 		private double bearing = 0.0;
 		
 		//constructor
-		public WP(String la, String lo, double dis, double bear){
+		public WP(String n, String la, String lo, double dis, double bear){
+			this.name = n;
 			this.latitude = la;
 			this.longitude = lo;
 			this.distance = dis;
 			this.bearing = bear;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
 		}
 
 		public String getLatitude() {
