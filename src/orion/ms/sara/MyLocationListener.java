9114af1b3c1861cebb0de0e93c5939522dab4515
xamieng <xami.eng@gmail.com>
2014-04-11 13:54:45 +0200

    add current location attribute

--- src/orion/ms/sara/MyLocationListener.java
+++ src/orion/ms/sara/MyLocationListener.java
@@ -26,6 +26,9 @@ public class MyLocationListener extends Activity implements LocationListener {
 	static double WaypointLatitude = 999;
 	static double WaypointLongitude = 999;
 	
+	static double currentLatitude = 0.0;
+	static double currentLongitude = 0.0;
+	
 	static float distance[] = new float[1];
 	static double bearing = 0.0;
 	
@@ -78,6 +81,8 @@ public class MyLocationListener extends Activity implements LocationListener {
 
 	@Override
 	public void onLocationChanged(Location loc) {
+		currentLatitude = loc.getLatitude();
+		currentLongitude = loc.getLongitude();
 		Resources resource = MainActivity.getContext().getResources();
 		speed = String.valueOf(Utils.arrondiSpeed(loc.getSpeed() * (1.945)));
 		heading = String.valueOf((int) loc.getBearing());
