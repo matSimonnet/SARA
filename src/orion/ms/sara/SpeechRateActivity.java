59e4d2b9dba17971bb8b3064d15bae4a5ab4055c
kvlch <kvlchankrachang@gmail.com>
2014-04-22 14:18:28 +0200

    #67 update layout

--- src/orion/ms/sara/SpeechRateActivity.java
+++ src/orion/ms/sara/SpeechRateActivity.java
@@ -3,11 +3,23 @@ package orion.ms.sara;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.NumberPicker;
+import android.widget.TextView;
 
 public class SpeechRateActivity extends Activity {
+	//layout components
+	private TextView speechText = null;
+	private TextView rate = null;
+	
+	//speech
+	private TextToSpeech tts = null;
 	private float speechRate = 2.0f;
+
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +29,26 @@ public class SpeechRateActivity extends Activity {
 		
 		//set default as the old value
 		this.speechRate = GeneralSettingActivity.speechRate;
+		
+		//OnInitListener Creation
+		OnInitListener onInitListener = new OnInitListener() {
+			@Override
+			public void onInit(int status) {
+			}
+		};
+		
+	    // textToSpeech creation
+		tts = new TextToSpeech(this, onInitListener);
+		tts.setSpeechRate(GeneralSettingActivity.speechRate);
+		
+		//textView
+		speechText = (TextView) findViewById(R.id.textView1);
+		speechText.setContentDescription("Adjust the speech rate below");
+		
+		//rate picker
+		rate = (TextView) findViewById(R.id.textView2);
+		rate.setContentDescription("speech rate");
+		
 		
 	}
 
