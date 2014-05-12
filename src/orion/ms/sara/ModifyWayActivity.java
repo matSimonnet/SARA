e7a5ee3e8969d0487133d2a20a0201ed6ee95e13
kvlch <kvlchankrachang@gmail.com>
2014-05-12 18:15:48 +0200

    #79 Bug fixes

--- src/orion/ms/sara/ModifyWayActivity.java
+++ src/orion/ms/sara/ModifyWayActivity.java
@@ -431,27 +431,53 @@ public class ModifyWayActivity extends Activity {
 	//set way point to way
 	@SuppressWarnings("static-access")
 	private void setWPtoWay(int spinnerID,WP wp){
+		int order = 3;
 		if(spinnerID==wp1List.getId()){
 			//way point1
-			temp.setWP(0, wp);
-			tts.speak("Waypoint1"+" is "+selecting.getName(), tts.QUEUE_FLUSH, null);
+			if(temp.getWP(0)!=null){
+				//replace old way point
+				temp.setWP(0, wp);
+			}
+			else{
+				//add new way point
+				temp.addWPtoWay(selecting);
+			}
+			order = 1;
 		}
 		else if(spinnerID==wp2List.getId()){
 			//way point2
-			temp.setWP(1, wp);
-			tts.speak("Waypoint2"+" is "+selecting.getName(), tts.QUEUE_FLUSH, null);
+			if(temp.getWP(1)!=null){
+				//replace old way point
+				temp.setWP(1, wp);
+			}
+			else{
+				//add new way point
+				temp.addWPtoWay(selecting);
+			}
+			order = 2;
 		}
 		else{
 			//old way points
 			for(int k=0;k<temp.getSize();k++){
 				if(spinnerID==(k+1)){
-					temp.setWP(k, wp);
-					tts.speak("Waypoint"+(k+1)+" is "+selecting.getName(), tts.QUEUE_FLUSH, null);
+					if(temp.getWP(k)!=null){
+						//replace old way point
+						temp.setWP(k, wp);
+					}
+					else{
+						//add new way point
+						temp.addWPtoWay(selecting);
+					}
+					order = k+1;
+					
 				}
 			}
-			//add new way point
-			temp.addWPtoWay(selecting);
+			if(order>temp.getSize()){
+				temp.addWPtoWay(selecting);
+			}
 		}
+		//tts announcement
+		tts.speak("Waypoint"+order+" is "+selecting.getName(), tts.QUEUE_FLUSH, null);
 	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
